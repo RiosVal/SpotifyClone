5ef3db1f6900abe09e6f3c3d37a76970c1c79224
Alejnadra Diez <pelu1033@gmail.com>
2024-10-22 10:38:23 -0500

    botón funcional para los artistas

--- src/components/artistComponent/ArtistComponent.jsx
+++ src/components/artistComponent/ArtistComponent.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom"; // Importar useNavigate para la navegación
 import "./ArtistComponent.scss";
 
-export function ArtistComponent({ artistName, photoURL, onArtistClick }) {
+export function ArtistComponent({ artistName, photoURL }) {
+    const navigate = useNavigate(); // Inicializar la navegación
+
+    const handleArtistClick = () => {
+        const formattedName = artistName.replace(/\s+/g, "%20"); // Formatear espacios
+        navigate(`/artist/${formattedName}`); // Redirigir a la ruta del artista
+    };
+
     return (
-        <button className="artist-component-button" onClick={() => onArtistClick(artistName)}>
-            <img className="artist-img" src={photoURL} alt="" />
+        <button className="artist-component-button" onClick={handleArtistClick}>
+            <img className="artist-img" src={photoURL} alt={artistName} />
             <div className="artist-info">
                 <p className="artist-name">{artistName}</p>
                 <p className="artist-label">Artista</p>
@@ -12,4 +20,3 @@ export function ArtistComponent({ artistName, photoURL, onArtistClick }) {
         </button>
     );
 }
-
